4f77fc5ae92c5594e75ef42a355d703c507f052c
Owain <owainjones@acadamytrainee.com>
2019-09-09 16:15:28 +0100

    Pokemon Repository added Pokemon model has been created Base stats model needs to be created

--- src/main/java/com/QA/model/Pokemon.java
+++ src/main/java/com/QA/model/Pokemon.java
@@ -12,6 +12,40 @@ public class Pokemon {
     Long id;
     String name;
     String[] type;
+    Long baseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getType() {
+        return type;
+    }
+
+    public void setType(String[] type) {
+        this.type = type;
+    }
+
+    public Long getBaseId() {
+        return baseId;
+    }
+
+    public void setBaseId(Long baseId) {
+        this.baseId = baseId;
+    }
+
 
 
 }

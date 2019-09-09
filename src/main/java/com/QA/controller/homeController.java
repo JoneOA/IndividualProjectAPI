d6291eef8ed9c3970ce7a2ad8dedfca7ef842d7a
Owain <owainjones@acadamytrainee.com>
2019-09-09 15:42:05 +0100

    File structure changes

---
+++ src/main/java/com/QA/controller/homeController.java
@@ -0,0 +1,14 @@
+package com.QA.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "hello world";
+    }
+
+}

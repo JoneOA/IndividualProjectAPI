be9f9e240220c059a5fc3ffc31a8108ca8986880
Owain <owainjones14@hotmail.co.uk>
2019-09-23 20:42:07 +0100

    Updated Inventory Repository to be able to seach by name Created Team Controller for later use

--- src/main/java/com/QA/repository/InventoryRepository.java
+++ src/main/java/com/QA/repository/InventoryRepository.java
@@ -4,4 +4,5 @@ import com.QA.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Inventory findByName(String name);
 }
